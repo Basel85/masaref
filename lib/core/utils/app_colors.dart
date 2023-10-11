c6fe8a816b4f4e51247deb314c7b4217cfba203e
Basel85 <baselbasel1515@gmail.com>
2023-10-11 11:17:56 +0300

    onboarding screen almost done

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,4 +7,5 @@ class AppColors {
   static const Color colorGrey = Colors.grey;
   static const Color colorPurple = Color(0xFF2E2A4F);
   static const Color colorLightBlue = Color(0xFF86C3E0);
+  static const Color colorYellow = Color(0xFFF9E79F);
 }
--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -27,7 +27,7 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-              builder: (_) => const CategoriesScreen()));
+              builder: (_) => const OnBoardingScreen()));
     });
     super.initState();
   }
